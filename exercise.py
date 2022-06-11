46b141f502cd1b5a00c5b27a04e98f1633555879
hasniera <107098926+hasniera@users.noreply.github.com>
2022-06-11 22:09:30 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
+# Data of Attended Private School
+This app predicts the **student enroll private school **!
 """)
 
 st.sidebar.header('User Input Parameters')
